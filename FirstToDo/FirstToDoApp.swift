686ed87183f91f4d6825913c4993391275ac9c21
asasakk <asasakkfml@gmail.com>
2025-11-26 09:57:31 +0900

    Initial Commit

---
+++ FirstToDo/FirstToDoApp.swift
@@ -0,0 +1,32 @@
+//
+//  FirstToDoApp.swift
+//  FirstToDo
+//
+//  Created by asai on 2025/11/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FirstToDoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
